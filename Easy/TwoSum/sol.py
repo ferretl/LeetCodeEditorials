16750f5044866121b595b8f0734964b7cb8a76e0
Arlie <Arlie4004@gmail.com>
2023-06-16 11:20:52 +1000

    added all 3 solutions

--- Easy/TwoSum/sol.py
+++ Easy/TwoSum/sol.py
@@ -1,3 +1,33 @@
+# Naive approach
+def twoSum(nums: list[int], target: int) -> list[int, int]:
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j and nums[i] + nums[j] == target:
+                return [i,j]
+    # by Leetcode specs we won't ever reach here but still good to have
+    return [-1,-1]   
+
+# Sorting approach
+def twoSum(nums: list[int], target: int) -> list[int, int]:
+    for i in range(len(nums)):
+        nums[i] = [nums[i],i]
+    nums = sorted(nums, key = lambda x:x[0])
+
+    l,r = 0, len(nums)-1
+
+    while l < r:
+        s = nums[l][0] + nums[r][0]
+        if s < target:
+            l += 1
+        elif s > target:
+            r -= 1
+        else:
+            return [nums[l][1], nums[r][1]]
+    # by Leetcode specs we won't ever reach here but still good to have
+    return [-1,-1] 
+
+
+# HaspMap approach
 def twoSum(nums: list[int], target: int) -> list[int, int]:
     for i in range(len(nums)):
         nums[i] = [nums[i],i]
@@ -13,7 +43,3 @@ def twoSum(nums: list[int], target: int) -> list[int, int]:
             return [nums[l][1], nums[r][1]]
     # by Leetcode specs we won't ever reach here but still good to have
     return [-1,-1]
-
-nums = [1,5,7,2,9]
-target = 3
-print(twoSum(nums,target))
